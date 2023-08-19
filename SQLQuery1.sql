bd28614285b879c77e1125bb202d45f9eba9c6ab
spl1tOne <levonvlad144@gmail.com>
2023-08-19 11:49:45 +0300

    Some Files Added

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -1,47 +1,59 @@
--- PhoneBook
-CREATE TABLE PhoneBook_Table (
-    contactID INT PRIMARY KEY,
-    FirstName TEXT,
-    LastName TEXT,
-    PhoneNumber TEXT,
-    Email TEXT,
-    Address TEXT
-);
+INSERT INTO Persons_Table (Id,FirstName,LastName,Age,Address,Gender)
+VALUES(9,'Huxley','Cook',22,'Kyiv','Male')
 
--- SchoolSchedule
-CREATE TABLE SchoolSchedule_Table (
-    scheduleID INT PRIMARY KEY,
-    ClassName TEXT,
-    TeacherName TEXT,
-    DayOfWeek TEXT,
-    startTime TIME,
-    endTime TIME
-);
+INSERT INTO Persons_Table (Id,FirstName,LastName,Age,Address,Gender)
+VALUES(2,'Diego','Howard',45,'Chicago','Male')
 
--- UserLoginHistory
-CREATE TABLE UserLoginHistory_Table (
-    loginId INT PRIMARY KEY,
-    userId INT,
-    loginTime DATETIME,
-    logoutTime DATETIME,
-    IpAddress TEXT,
-	UserLocation TEXT
-);
+INSERT INTO Persons_Table (Id,FirstName,LastName,Age,Address,Gender)
+VALUES(3,'Jorge','Young',19,'Dublin','Female')
 
--- BankAccounts
-CREATE TABLE BankAccounts_Table (
-    accountId INT PRIMARY KEY,
-    AccountNumber TEXT,
-    userId INT,
-    userBalance DECIMAL,
-    investingDate DATE
-);
+INSERT INTO Persons_Table (Id,FirstName,LastName,Age,Address,Gender)
+VALUES(4,'Calvin','Rodriguez',17,'Dubai','Male')
 
--- BankTransactionsData
-CREATE TABLE BankTransactionsData_Table (
-    transactionId INT PRIMARY KEY,
-    accountId INT,
-    TransactionType TEXT,
-    TransactionAmount DECIMAL,
-    transactionDate DATETIME
-);
+INSERT INTO Persons_Table (Id,FirstName,LastName,Age,Address,Gender)
+VALUES(10,'Xu','White',9,'Seoul','Male')
+
+INSERT INTO Persons_Table (Id,FirstName,LastName,Age,Address,Gender)
+VALUES(8,'Braydon','Green',24,'Venice','Female')
+
+INSERT INTO Persons_Table (Id,FirstName,LastName,Age,Address,Gender)
+VALUES(7,'Yaqub','Allen',34,'Tokyo','Male')
+
+INSERT INTO Persons_Table (Id,FirstName,LastName,Age,Address,Gender)
+VALUES(5,'Neil','Baker',31,'Warsaw','Male')
+
+INSERT INTO Persons_Table (Id,FirstName,LastName,Age,Address,Gender)
+VALUES(1,'Valentin','Hayes',16,'Stockholm','Female')
+
+INSERT INTO Persons_Table (Id,FirstName,LastName,Age,Address,Gender)
+VALUES(6,'Kaden','Hayes',23,'Singapore','Male')
+
+INSERT INTO Persons_Table (Id,FirstName,LastName,Age,Address,Gender)
+VALUES(12,'Philip','Gonzalez',35,'Shanghai','Male')
+
+INSERT INTO Persons_Table (Id,FirstName,LastName,Age,Address,Gender)
+VALUES(11,'Gavin','Sanders',16,'Kyiv','Male')
+
+INSERT INTO Persons_Table (Id,FirstName,LastName,Age,Address,Gender)
+VALUES(13,'Emiliano','Allen',44,'New York','Male')
+
+INSERT INTO Persons_Table (Id,FirstName,LastName,Age,Address,Gender)
+VALUES(15,'Silas','Bell',22,'Madrid','Female')
+
+INSERT INTO Persons_Table (Id,FirstName,LastName,Age,Address,Gender)
+VALUES(17,'Yoshiaki','Flores',8,'London','Male')
+
+INSERT INTO Persons_Table (Id,FirstName,LastName,Age,Address,Gender)
+VALUES(20,'Raiden','Simmons',21,'Kyiv','Male')
+
+INSERT INTO Persons_Table (Id,FirstName,LastName,Age,Address,Gender)
+VALUES(14,'Titus','Moore',35,'Kyiv','Male')
+
+INSERT INTO Persons_Table (Id,FirstName,LastName,Age,Address,Gender)
+VALUES(18,'Tyrone','Powell',12,'Los Angeles','Female')
+
+INSERT INTO Persons_Table (Id,FirstName,LastName,Age,Address,Gender)
+VALUES(16,'Felipe','Lee',24,'Kyiv','Male')
+
+INSERT INTO Persons_Table (Id,FirstName,LastName,Age,Address,Gender)
+VALUES(19,'Yaqub','Anderson',18,'Berlin','Female')
