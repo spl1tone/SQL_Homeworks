6349ac6a003b1e891d644f7daba3c664ec76b8e0
V1ad <137112529+spl1tone@users.noreply.github.com>
2023-08-19 11:16:22 +0300

    1

---
+++ SQLQuery1.sql
@@ -0,0 +1,59 @@
+INSERT INTO Persons_Table (Id,FirstName,LastName,Age,Address,Gender)
+VALUES(9,'Huxley','Cook',22,'Kyiv','Male')
+
+INSERT INTO Persons_Table (Id,FirstName,LastName,Age,Address,Gender)
+VALUES(2,'Diego','Howard',45,'Chicago','Male')
+
+INSERT INTO Persons_Table (Id,FirstName,LastName,Age,Address,Gender)
+VALUES(3,'Jorge','Young',19,'Dublin','Female')
+
+INSERT INTO Persons_Table (Id,FirstName,LastName,Age,Address,Gender)
+VALUES(4,'Calvin','Rodriguez',17,'Dubai','Male')
+
+INSERT INTO Persons_Table (Id,FirstName,LastName,Age,Address,Gender)
+VALUES(10,'Xu','White',9,'Seoul','Male')
+
+INSERT INTO Persons_Table (Id,FirstName,LastName,Age,Address,Gender)
+VALUES(8,'Braydon','Green',24,'Venice','Female')
+
+INSERT INTO Persons_Table (Id,FirstName,LastName,Age,Address,Gender)
+VALUES(7,'Yaqub','Allen',34,'Tokyo','Male')
+
+INSERT INTO Persons_Table (Id,FirstName,LastName,Age,Address,Gender)
+VALUES(5,'Neil','Baker',31,'Warsaw','Male')
+
+INSERT INTO Persons_Table (Id,FirstName,LastName,Age,Address,Gender)
+VALUES(1,'Valentin','Hayes',16,'Stockholm','Female')
+
+INSERT INTO Persons_Table (Id,FirstName,LastName,Age,Address,Gender)
+VALUES(6,'Kaden','Hayes',23,'Singapore','Male')
+
+INSERT INTO Persons_Table (Id,FirstName,LastName,Age,Address,Gender)
+VALUES(12,'Philip','Gonzalez',35,'Shanghai','Male')
+
+INSERT INTO Persons_Table (Id,FirstName,LastName,Age,Address,Gender)
+VALUES(11,'Gavin','Sanders',16,'Kyiv','Male')
+
+INSERT INTO Persons_Table (Id,FirstName,LastName,Age,Address,Gender)
+VALUES(13,'Emiliano','Allen',44,'New York','Male')
+
+INSERT INTO Persons_Table (Id,FirstName,LastName,Age,Address,Gender)
+VALUES(15,'Silas','Bell',22,'Madrid','Female')
+
+INSERT INTO Persons_Table (Id,FirstName,LastName,Age,Address,Gender)
+VALUES(17,'Yoshiaki','Flores',8,'London','Male')
+
+INSERT INTO Persons_Table (Id,FirstName,LastName,Age,Address,Gender)
+VALUES(20,'Raiden','Simmons',21,'Kyiv','Male')
+
+INSERT INTO Persons_Table (Id,FirstName,LastName,Age,Address,Gender)
+VALUES(14,'Titus','Moore',35,'Kyiv','Male')
+
+INSERT INTO Persons_Table (Id,FirstName,LastName,Age,Address,Gender)
+VALUES(18,'Tyrone','Powell',12,'Los Angeles','Female')
+
+INSERT INTO Persons_Table (Id,FirstName,LastName,Age,Address,Gender)
+VALUES(16,'Felipe','Lee',24,'Kyiv','Male')
+
+INSERT INTO Persons_Table (Id,FirstName,LastName,Age,Address,Gender)
+VALUES(19,'Yaqub','Anderson',18,'Berlin','Female')
